e74b60427537894b6813cc6d1f58d4dd2e584b1d
bhavesh-bari <bhaveshbari0402@gmail.com>
2025-10-31 21:17:39 +0530

    build error2

--- src/app/payment-success/page.jsx
+++ src/app/payment-success/page.jsx
@@ -1,11 +1,16 @@
+"use client";
+import { useSearchParams } from "next/navigation";
 import BookingConfirmed from "@/components/BookingConfirmed";
 import Navbar from "@/components/Navbar";
 
 export default function PaymentSuccessPage() {
+    const searchParams = useSearchParams();
+    const id = searchParams.get("id");
+
     return (
         <div className="flex flex-col min-h-screen bg-white">
             <Navbar />
-            <BookingConfirmed />
+            <BookingConfirmed bookingId={id ?? ""} />
         </div>
     );
 }
